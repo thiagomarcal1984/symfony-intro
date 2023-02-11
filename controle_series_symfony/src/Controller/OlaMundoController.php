5c966513799a1f3cceb58b90ce340f075af613b6
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-02-11 11:34:28 -0300

    Lidando com Request e Response

--- controle_series_symfony/src/Controller/OlaMundoController.php
+++ controle_series_symfony/src/Controller/OlaMundoController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OlaMundoController extends AbstractController
 {
-    #[Route('/ola_mundo', name: 'app_ola_mundo')]
-    public function index(): Response
+    #[Route('/ola_mundo')]
+    public function index(Request $request): Response
     {
-        return new Response("Olá, mundo!"); 
+        return new Response(
+            "<h1>Olá, mundo!</h1>" . $_GET['id']
+        ); 
     }
 }
